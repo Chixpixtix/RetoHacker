673013860e61526530d15fd8ed7c682b47aaf6e8
Chixpixtix <formosoagostina@gmail.com>
2025-02-03 13:53:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // script.js
 function checkPassword() {
-    let password = document.getElementById("password").value;
+    let password = document.getElementById("password").value.trim(); // .trim() elimina espacios antes y después
     if (password === "3xpl0it_2025") {
         window.location.href = "nivel2.html";  // Redirige al siguiente nivel
     } else {
         document.getElementById("message").innerHTML = "⚠️ Acesso negado. Tente novamente.";
     }
 }
-
-
